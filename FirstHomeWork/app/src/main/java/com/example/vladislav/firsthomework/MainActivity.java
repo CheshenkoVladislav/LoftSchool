6363678ec91dc7344b5b96fc71992ec6efae1bab
PavelEskavator <epotajni1488@gmail.com>
2018-03-08 23:48:28 +0300

    HomeWork

--- FirstHomeWork/app/src/main/java/com/example/vladislav/firsthomework/MainActivity.java
+++ FirstHomeWork/app/src/main/java/com/example/vladislav/firsthomework/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.vladislav.firsthomework;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("start","СТАРРРТУЕМ!!");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("resume","ИЛИ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("pause","П*ДУЕЕЕММ!!");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("stop", "СТОП");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("destroy", "МАШИНА");
     }
 }
