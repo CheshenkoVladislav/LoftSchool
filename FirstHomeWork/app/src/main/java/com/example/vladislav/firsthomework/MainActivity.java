3151c9353fade469f57f3759892fd958e162a6c9
PavelEskavator <epotajni1488@gmail.com>
2018-03-11 11:53:56 +0300

    secondHomeWork

--- FirstHomeWork/app/src/main/java/com/example/vladislav/firsthomework/MainActivity.java
+++ FirstHomeWork/app/src/main/java/com/example/vladislav/firsthomework/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.vladislav.firsthomework;
 
+import android.*;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.util.*;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ MyApplication/app/src/main/java/com/example/vladislav/myapplication/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.vladislav.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private EditText name;
+    private EditText price;
+    private Button addPosition;
+    private boolean nameActive = false;
+    private boolean priceActive = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.name);
+        price = findViewById(R.id.price);
+        addPosition = findViewById(R.id.addPosition);
+
+        TextWatcher g = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if (name.getText().length() != 0 && price.getText().length() != 0)addPosition.setEnabled(true);
+                else addPosition.setEnabled(false);
+
+            }
+        };
+        name.addTextChangedListener(g);
+        price.addTextChangedListener(g);
+
+        addPosition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    String itemName = name.getText().toString();
+                    String itemPrice = price.getText().toString();
+            }
+        });
+    }
+
+}
